a0919b0a3c03e69ae95e037d71779746792ef53f
yllan <yllan@me.com>
2013-09-02 15:24:47 +0800

    Fix scala 2.10.2 build settings

--- project/Build.scala
+++ project/Build.scala
@@ -13,14 +13,13 @@ object ApplicationBuild extends Build {
       "postgresql" % "postgresql" % "9.2-1002.jdbc4"
     )
 
-    scalaVersion := "2.10.2"
-
     val main = play.Project(appName, appVersion, appDependencies).settings(
       resolvers ++= Seq(
         "jBCrypt Repository" at "http://repo1.maven.org/maven2/org/",
         "fwbrasil.net" at "http://fwbrasil.net/maven/",
         "Typesafe Repo" at "http://repo.typesafe.com/typesafe/repo/"
-      )
+      ),
+      scalaVersion := "2.10.2"
     )
 
 }
