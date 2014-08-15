cdf7c950a81d1e521e7bec5154ac231789c20c37
Yung-Luen Lan <yllan@me.com>
2014-08-15 12:46:26 +0800

    Remove integration spec

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -13,15 +13,15 @@ class IntegrationSpec extends Specification {
   
   "Application" should {
     
-    "work from within a browser" in {
-      running(TestServer(3333), HTMLUNIT) { browser =>
-
-        browser.goTo("http://localhost:3333/mobile")
-
-        browser.pageSource must contain("")
-       
-      }
-    }
+//    "work from within a browser" in {
+//      running(TestServer(3333), HTMLUNIT) { browser =>
+//
+//        browser.goTo("http://localhost:3333/mobile")
+//
+//        browser.pageSource must contain("")
+//
+//      }
+//    }
     
   }
   
