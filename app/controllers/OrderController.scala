84c63a15f9cbdece2ec9030f2a3ee7650e3a19df
Yung-Luen Lan <yllan@me.com>
2014-08-15 16:55:52 +0800

    Remove unused action.

--- app/controllers/OrderController.scala
+++ app/controllers/OrderController.scala
@@ -22,30 +22,6 @@ object OrderController extends Controller {
   import play.api.Play.current
   import models.PiecesDAO._
 
-  val orderForm = Form(tuple("name" -> text.verifying(nonEmpty), "address" -> text.verifying(nonEmpty)))
-
-  def saveAddress(orderNumber: String) = Action.async { implicit request =>
-    Future {
-      DB.withSession { implicit session =>
-        Pieces.filter(p => p.token === orderNumber).firstOption.map(p => {
-          orderForm.bindFromRequest.fold(
-            formWithErrors => {
-              Logger.info("error: " + formWithErrors.errorsAsJson)
-              BadRequest(formWithErrors.errorsAsJson)
-            }, 
-            value => {
-              val (name, address) = value
-              Logger.info(s"Receive: $name, $address")
-              Ok(views.html.order(p, orderForm))
-            }
-          )            
-        }).getOrElse(
-          NotFound(Json.obj("error" -> "Pieces Not Found"))
-        )
-      }
-    }
-  }
-
   def makeOrder = Action.async(parse.tolerantJson(maxLength = 2 * 1024 * 1024)) { request =>
     Logger.info("receive: " + request.body)
     Json.fromJson[Piece](request.body).fold(
@@ -65,7 +41,7 @@ object OrderController extends Controller {
     Future {
       DB.withSession { implicit session =>
         Pieces.filter(p => p.token === orderToken).firstOption.map(p =>
-          Ok(views.html.order(p, orderForm))
+          Ok(views.html.order(p))
         ).getOrElse(
           NotFound("Not Found!")
         )
