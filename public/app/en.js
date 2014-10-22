7880c2ee2fc7f57983bf9bfbf3abe59e74ac8499
Yung-Luen Lan <yllan@me.com>
2014-10-22 23:04:19 +0800

    Add en.js

---
+++ public/app/en.js
@@ -0,0 +1,69 @@
+_.setTranslation({
+    /* Confirmation */
+    "COUPON_CONFIRMING": "Redeeming…",
+    "TRY_AGAIN": "Connection Failed. Try later.",
+    "COUPON_INVALID": "This coupon is invalid.",
+    "SHIPPING": "Shipping",
+    "DISCOUNT": "Discount",
+    "TOTAL": "Total",
+    "INPUT_COUPON": "Coupon Code",
+    "COUPON_CONFIRM": "Use",
+    "COUPON_USE": "Redeemed",
+    "ESTIMATED_SHIPPING_DATE": "Estimated Ship Date",
+
+    /* Credit Card */
+    "CREDIT_CARD_DESCRIPTION_1": "You can use",
+    "CREDIT_CARD_DESCRIPTION_2": "or",
+    "CREDIT_CARD_DESCRIPTION_3": "to pay.",
+    "CREDIT_CARD_NO": "Card number",
+    "CREDIT_CARD_EXPIRY": "Expiry date",
+    "CREDIT_CARD_SECURE_CODE": "CVV2",
+    "SECURE_CODE_EXPLANATION": "Three digits on the back",
+
+    /* Done */
+    "YOUR_ORDER_NO": "Your order number",
+    "ORDER_SUCCESSFUL": " Order complete",
+    "TRACK_ORDER": "Please track your order here",
+    
+    /* Login */
+    "EMAIL_PLACEHOLDER": "EMAIL",
+    "PASSWORD_PLACEHOLDER": "PASSWORD",
+    "LOGIN": "Login",
+    "SIGNUP": "Sign Up",
+    "GO_SIGNUP_ACCOUNT": "No account? Sign Up Now.",
+
+    /* Shipping */
+    "RECEIVER": "Receiver’s Name",
+    "RECEIVER_PHONE": "Receiver’s Phone",
+    "CITY": "City",
+    "AREA": "Area",
+    "POSTAL_CODE": "Postal Code",
+    "ADDRESS": "Address",
+
+    /* Sign Up */
+    "NAME": "Name",
+    "TEL": "Phone",
+    "EMAIL": "Email",
+    "EMAIL_EXPLANATION": "We’ll send you a mail to confirm.",
+    "PASSWORD": "Password",
+    "PASSWORD_CONFIRM": "Confirm Password",
+
+    /* App */
+    "TITLE.LOGING": "Login",
+    "TITLE.SIGNUP": "Sign Up",
+    "TITLE.SIGNUP.CONFIRM": "Ok",
+    "TITLE.SHIPPING": "Ship Info",
+    "TITLE.SHIPPING.NEXT": "Next",
+    "TITLE.CONFIRMATION": "Order Info",
+    "TITLE.CREDITCARD": "Pay by Credit Card",
+    "TITLE.CREDITCARD.CONFIRM": "Pay",
+    "TITLE.DONE": "Order Complete",
+    "TITLE.DONE.DONE": "Done",
+    "TITLE.NOT_FOUND": "Not found",
+
+    /* Action */
+    "LOGIN_ERROR": "Login error. Try later.",
+    "PASSWORD_NOT_MATCH": "Wrong password twice. Try later.",
+    "SIGNUP_ERROR": "Sign up error. Try later.",
+    "GENERAL_ERROR": "Connection failed. Try later.",
+});
